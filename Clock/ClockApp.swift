d526df992a690d264d171d3a6f45af48a60cac1d
Joseph Ji <jji@lcs.on.ca>
2024-11-08 08:52:10 -0500

    Initial Commit

---
+++ Clock/ClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClockApp.swift
+//  Clock
+//
+//  Created by Xinchen Ji on 2024-11-08.
+//
+
+import SwiftUI
+
+@main
+struct ClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
